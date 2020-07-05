56f2941b669c46d52a1b45d81fd9fcad65537800
Toxa xa <linn84@mail.ru>
2020-07-06 00:22:19 +0300

    bg color

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,18 +4,21 @@ import pygame
 def run_game():
 
     #Инициализирует игру и создает обьект окна
+
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
-    #  Подпись окна
+
+    screen = pygame.display.set_mode((800,600))
     pygame.display.set_caption("Alien invasion")
-
- #Запуск основного цикла игры
-
+    # Запуск основного цикла игры
+    bg_color = (230, 230, 230)
     while True:
         # отслеживание событий клавиатуры и мыши
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        # При каждом проходе цикла перерисовываеться экран
+        screen.fill(bg_color)
+
 
     #Отображение последнего прорисованого экрана
     pygame.display.flip()
