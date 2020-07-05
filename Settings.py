d4293ae1072581be3e4336ac9d2a7254bc2161a6
Toxa xa <linn84@mail.ru>
2020-07-06 00:54:37 +0300

    add Settings.py

---
+++ Settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """Клас для хранения всех настроек игры Alien Invasion"""
+    def __init__(self):
+        """инициализирует гастройки игры"""
+        self.screen_width = 1200
+        self.screen_height =800
+        self.bg_color = (230, 230, 230)
